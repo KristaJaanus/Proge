616538b8dc33f976b8e069de240163206723ece4
kristajaanus <krista.jaanus@tptlive.ee>
2017-10-23 15:38:27 +0300

    teine

---
+++ src/08_exercise/08_exercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] excuses = new[] { "Koer sõi kodutöö ära", "Ema ei luba arvutit kasutada" };
+            for (int i = 0; i < excuses.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}.{excuses[i]}");
+            }
+            Console.ReadLine();
+                }
+    }
+}
